11fa459662c24394be8c0581ad40157bf23b8ab8
594462206@qq.com <594462206@qq.com>
2016-08-10 22:18:04 +0800

    增加聊天页面 @chenyujing

---
+++ thunk/client/android/xiaxiang/app/src/main/java/org/xiaxiang/xiaxiang/activity/ChatActivity.java
@@ -0,0 +1,14 @@
+package org.xiaxiang.xiaxiang.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.xiaxiang.xiaxiang.R;
+
+public class ChatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
